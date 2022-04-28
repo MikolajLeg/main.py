95f2463f0032a22cd933b03d561ee3a34c628b7d
MikolajLeg <01169515@pw.edu.pl>
2022-04-28 14:50:23 +0200

    list?

--- main.py
+++ main.py
@@ -80,9 +80,9 @@ class Kraj:
         atrybuty = {k.split("__")[-1]: v for k, v in self.__dict__.items()}
         return f"{nazwa}: {atrybuty} "
 
-
 if __name__ == '__main__':
-    dane = read_file('Electricity prices for household consumers - bi-annual data (from 2007 onwards) [NRG_PC_204].csv')
+    dane = read_file(
+        'Electricity prices for household consumers - bi-annual data (from 2007 onwards) [NRG_PC_204].csv')
     print(dane)
 
     Belgium = Kraj("Belgium", dane)
@@ -99,3 +99,6 @@ if __name__ == '__main__':
 
     Spain = Kraj("Spain", dane)
     print(Spain)
+
+    kos = Kraj("Kosovo",dane)
+    print(kos)
